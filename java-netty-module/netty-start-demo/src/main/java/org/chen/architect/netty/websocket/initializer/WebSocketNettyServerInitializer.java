a76e793767d8ab1b02f5bdbeb95d180ea496196d
chensj(陈世杰) <chensj@winning.com.cn>
2019-07-12 20:48:10 +0800

    comment

--- java-netty-module/netty-start-demo/src/main/java/org/chen/architect/netty/websocket/initializer/WebSocketNettyServerInitializer.java
+++ java-netty-module/netty-start-demo/src/main/java/org/chen/architect/netty/websocket/initializer/WebSocketNettyServerInitializer.java
@@ -22,15 +22,15 @@ public class WebSocketNettyServerInitializer extends ChannelInitializer<SocketCh
     protected void initChannel(SocketChannel ch) throws Exception {
 
         ChannelPipeline pipeline = ch.pipeline();
-
+        // http 处理器
         pipeline.addLast(new HttpServerCodec());
-
+        // chunk 写处理器
         pipeline.addLast(new ChunkedWriteHandler());
-        // netty 中将数据分块来处理 ，在这个处理器会合并，当超过最大size，还是会调用另一个方法
+        // netty 中将数据分块来处理 ，在这个处理器会合并，当超过最大maxContentLength，还是会调用另一个方法 handleOversizedMessage
         pipeline.addLast(new HttpObjectAggregator(8192));
-
+        // WebSocket请求的根路径  ws://localhost:8080/ws
         pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
-
+        // 自定义处理器
         pipeline.addLast(new TextWebSocketNettyServerHandler());
     }
 }
